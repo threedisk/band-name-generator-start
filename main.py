867276d4013dc24ede64f3c5678f86d8c688793a
threedisk <threedisk@gmail.com>
2020-12-29 10:53:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print ("Hello "+input ("What is your name?\n "))
+#2. Ask the user for the city that they grew up in.
+kota = input ("Anda lahir di kota mana? \n")
+#3. Ask the user for the name of a pet.
+hewan = input ("Nama hewan peliharaan Anda?\n ")
+#4. Combine the name of their city and pet and show them their band name.
+namaBand= kota +" " + hewan
+print ("Nama band Anda adalah "+namaBand)
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
